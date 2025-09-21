5cc33f74313d55d7b0f3128702da417a1db64a21
ImWojtek <wawrzynskiwojciech@gmail.com>
2025-09-21 14:31:51 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import sys
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+from functions.get_file_info import get_files_info
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+
+user_prompt = sys.argv[1]
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)])
+]
+response = client.models.generate_content(
+    model = "gemini-2.0-flash-001",
+    contents = messages,
+)
+prompt_tokens = response.usage_metadata.prompt_token_count
+response_tokens = response.usage_metadata.candidates_token_count
+
+if len(sys.argv) < 2:
+    sys.exit("Please provide a prompt as a command-line argument.")
+if sys.argv[2] == "--verbose":
+    print(f"User prompt: {user_prompt}")
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(response.text)
+    print(f"Response tokens: {response_tokens}")
